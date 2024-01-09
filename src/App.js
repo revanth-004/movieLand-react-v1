9d51cf6d6a49fad2e08f5c54fcc5ab59def411df
Revanth kumar S <98330771+revanth-004@users.noreply.github.com>
2024-01-09 16:36:53 +0530

    Update App.js -HTTPs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
 
-const API_URL = "http://www.omdbapi.com?apikey=60adf5d6";
+const API_URL = "https://www.omdbapi.com?apikey=60adf5d6";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
